22c4f6301e100f07fdb4672a9579ab963d082d24
Poboxx Lbteksystems <poboxx@lbteksystems.com>
2023-09-20 23:56:47 +0800

    Model instances 1

--- index.js
+++ index.js
@@ -42,12 +42,35 @@ const User = sequelize.define('user', {
     timestamps: false
 });
 
-User.sync({ force: true });
-// User.drop();
-// console.log(sequelize.models.user);
+// User.sync({ force: true });
+User.sync({ alter: true }).then(() =>{
+    //Working with our updated table
+    return User.create({
+        username: 'WittCode',
+        // password: 'subscribe',
+        // age: 25,
+        // WittCodeRocks: false
+    });
+    // const user = User.build({ username: 'WittCode', password: '123', age: 25, WittCodeRocks: true });
+    // user.username = 'soccer';
+    // return user.save();
+})
+.then((data) =>{
+    // console.log('User added to database');
+    // data.username = 'pizza';
+    // data.age = 45;
+    // return data.save({ fields: ['age'] });
+    // return data.reload();
+    // return data.save();
+    // return data.destroy();
+    // data.decrement({ age: 2 })
+    console.log(data.toJSON());
 
-// User.sync({ alter: true }).then(() => {
-//     console.log("Connection successful!");
-// }).catch((err) =>{
-//     console.log("Error connecting database!");
+})
+// .then((data) =>{
+//     console.log('User updated!');
+//     console.log(data.toJSON());
 // })
+.catch((err) => {
+    console.log(err);
+})
